7fbc0e13d415fcf4355abc22caa98e7f0443409e
hindigarv <hindi.garv.001@gmail.com>
2022-11-04 17:29:59 +0100

    implement the generator

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-export function generateValuesFromRegex(regex) {
-    return ["test"]
+export function generateValuesFromRegex(pattern) {
+    let index = 0;
+    let words = [""];
+
+    let getNextOptions = function() {
+        let options = []
+        let currOption = "";
+        while (pattern[index] !== ')') {
+            let ch = pattern[index]
+            index++
+            if (ch === '|') {
+                options.push(currOption)
+                currOption = ""
+                continue
+            }
+            currOption += ch;
+        }
+        options.push(currOption)
+        index++
+        if (pattern[index] === '?') {
+            options.push("")
+            index++
+        }
+        return options
+    }
+
+    while (pattern[index] !== '$') {
+        let ch = pattern[index]
+        index++
+
+        if (ch === '^') continue
+        if (ch === '(') {
+            let options = getNextOptions()
+            if (options) {
+                words = words
+                    .flatMap(r => options.map(o => r + o) )
+            }
+            continue
+        }
+        words = words.map(w => w + ch)
+    }
+
+    return words
 }
